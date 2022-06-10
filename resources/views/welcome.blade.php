3d45560a4f5be1c0f20ead62bb8cab78699f20d0
Soukaina BENDAOUD <53907319+soukai-na@users.noreply.github.com>
2022-06-10 14:25:07 +0100

    interface

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('base')
+@section('content')
+    
+
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row mb-3">
+            <div class="col-sm-12 mb-4 mb-xl-0">
+              <h4 class="font-weight-bold text-dark">Hi, welcome back!</h4>
+            </div>
+          </div>
+         hnaa!!
+        </div>
+        <!-- content-wrapper ends -->
+        
+      </div>
+      <!-- main-panel ends -->
+   
+@endsection
