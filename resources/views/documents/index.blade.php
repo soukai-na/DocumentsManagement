3a60aba3a0687d0d3fe19850af5c7580852c6e19
Soukaina BENDAOUD <53907319+soukai-na@users.noreply.github.com>
2022-06-16 09:02:10 +0100

    firstsearch

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -1,67 +1,69 @@
 @extends('base2')
 
 @section('content')
-      <div class="main-panel">
+    <div class="main-panel">
         <div class="content-wrapper">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="{{ route('welcome')}}">Page d'accueil</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Documents</li>
+                    <li class="breadcrumb-item"><a href="{{ route('welcome') }}">Page d'accueil</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Documents</li>
                 </ol>
-              </nav>
-          <div class="row">
-            <div class="col-lg-12 grid-margin stretch-card">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title"></h4>
-                    <div class="table-responsive">
-                      <table class="table table-striped">
-                        <thead>
-                          <tr>
-                            <th>Designation </th>
-                            <th>Description</th>
-                            <th>File</th>
-                            <th>Type</th>
-                            <th>Date de création</th>
-                          </tr>
-                        </thead>
-                        @foreach ($documents as $document)
-                        <tbody>
-                          <tr onclick="document.location='{{ route('documents.show',$document->id) }}'" style="cursor:pointer;">
-                            <td>
-                                {{ $document->designation }}
-                            </td>
-                            <td>
-                                {{ $document->description }}
-                            </td>
-                            <td class="py-1">
-                                <a target="_blank" href="{{ url('documents/'.$document->file) }}">{{$document->file}}</a>
-                            </td>
-                            <td>
-                                {{ $document->type }}
-                            </td>
-                            <td>
-                                {{ $document->created_at }}
-                            </td>
-                          </tr>
-                        </tbody>
-                        @endforeach
-                      </table>
+            </nav>
+            <div class="row">
+                <div class="col-lg-12 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title"></h4>
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Designation </th>
+                                            <th>Description</th>
+                                            <th>File</th>
+                                            <th>Type</th>
+                                            <th>Date de création</th>
+                                        </tr>
+                                    </thead>
+                                    @foreach ($documents as $document)
+                                        <tbody>
+                                            <tr onclick="document.location='{{ route('documents.show', $document->id) }}'"
+                                                style="cursor:pointer;">
+                                                <td>
+                                                    {{ $document->designation }}
+                                                </td>
+                                                <td>
+                                                    {{ $document->description }}
+                                                </td>
+                                                <td class="py-1">
+                                                    <a target="_blank"
+                                                        href="{{ url('documents/' . $document->file) }}">{{ $document->file }}</a>
+                                                </td>
+                                                <td>
+                                                    {{ $document->type }}
+                                                </td>
+                                                <td>
+                                                    {{ $document->created_at }}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    @endforeach
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                  </div>
+
+
+
+
+                    <div class='d-flex justify-content-center mt-5'>
+                        {{ $documents->links('vendor.pagination.custom') }}
+                    </div>
                 </div>
-              
-                
-                
-                
-              <div class='d-flex justify-content-center mt-5'>
-                  {{ $documents->links('vendor.pagination.custom') }}
-              </div>
+                <!-- content-wrapper ends -->
+
             </div>
             <!-- content-wrapper ends -->
-            
-          </div>
-        <!-- content-wrapper ends -->
-        
-      </div>
+        </div>
+    </div>
 @endsection
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -12,7 +12,43 @@
                 <div class="col-lg-12 grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
-                            <h1>Résultat de recherche pour:</h1>
+                            <h3>Résultat de recherche pour:</h3><br>
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Designation </th>
+                                            <th>Description</th>
+                                            <th>File</th>
+                                            <th>Type</th>
+                                            <th>Date de création</th>
+                                        </tr>
+                                    </thead>
+                                    @foreach ($documents as $document)
+                                        <tbody>
+                                            <tr onclick="document.location='{{ route('documents.show', $document->id) }}'"
+                                                style="cursor:pointer;">
+                                                <td>
+                                                    {{ $document->designation }}
+                                                </td>
+                                                <td>
+                                                    {{ $document->description }}
+                                                </td>
+                                                <td class="py-1">
+                                                    <a target="_blank"
+                                                        href="{{ url('documents/' . $document->file) }}">{{ $document->file }}</a>
+                                                </td>
+                                                <td>
+                                                    {{ $document->type }}
+                                                </td>
+                                                <td>
+                                                    {{ $document->created_at }}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    @endforeach
+                                </table>
+                            </div>
 
                         </div>
                     </div>
